2327f49becae4d5a73cd192dcb78be88e39985bb
jnnh <jenniferhorvath@cmail.carleton.ca>
2021-12-21 17:19:01 -0500

    linked two pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName){
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName;
+    }
+    else{
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function(repo){
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -14,6 +27,7 @@ var getRepoIssues = function(repo){
         }
         else{
             alert("there was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -51,4 +65,4 @@ var displayWarning = function(repo){
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
